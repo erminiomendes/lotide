3ad28107d22834a67effee0b69141e1bb80fdd02
Erminio Mendes <55849740+erminiomendes@users.noreply.github.com>
2021-07-19 17:30:56 -0700

    Implement assertEqual *

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let x = console.assert( actual + expected === 2 )
+  
+  if ( x === "failed" ){
+    console.log(`Assertion Failed: ${actual} !== ${expected} `);
+  }else {
+    console.log(`Assertion Passed: ${actual} === ${expected} `);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2, 1);
